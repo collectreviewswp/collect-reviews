ec90534e45b0b86a15d511b066e4d9874fdf52a0
Alexander Sakal <alexandersakal02@gmail.com>
2024-01-14 17:15:37 +0200

    Split react app node modules into separate chunks

--- react-app/craco.config.js
+++ react-app/craco.config.js
@@ -1,9 +1,30 @@
 module.exports = {
 	webpack: {
-		configure: {
-			externals: {
-				'@wordpress/i18n': [ 'window wp', 'i18n' ],
-			},
+		configure: (webpackConfig, { env, paths }) => {
+
+			webpackConfig.externals = {
+				'@wordpress/i18n': ['window wp', 'i18n'],
+			};
+
+			if( env === 'production' ) {
+				webpackConfig.optimization = {
+					splitChunks: {
+						// Include node_modules in the separate chunk.
+						chunks: 'all',
+						// Add "min.js" extension to the node_modules chunks to prevent those files parsing while translation (POT) file generation on translate.wordpress.org.
+						cacheGroups: {
+							defaultVendors: {
+								test: /[\\/]node_modules[\\/]/,
+								priority: -10,
+								reuseExistingChunk: true,
+								filename: 'static/js/[name].[contenthash:8].chunk.min.js',
+							},
+						},
+					},
+				};
+			}
+
+			return webpackConfig;
 		},
 	},
 	devServer: {
